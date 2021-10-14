7954d40d53dd6c538eec1dfd7e15bb7491f95fa9
artapresyan <apresyanarturv@gmail.com>
2021-10-14 23:06:51 +0400

    added validation for admins

--- src/main/java/com/workspace/workSpace/service/AdminService.java
+++ src/main/java/com/workspace/workSpace/service/AdminService.java
@@ -28,9 +28,16 @@ public class AdminService {
     }
 
     public String addAdmin(String adminUsername, String adminPassword, String adminEmail, String adminPhone){
-        Admin newAdmin=new Admin(adminUsername, adminPassword, adminEmail, adminPhone);
-        adminRepository.save(newAdmin);
-        return "Congratulations! "+adminUsername+" registered successfully.";
+        if (adminUsername.matches("[a-z]{7,}|[a-z]{3,}[a-z0-9]{4,}")
+                && adminPassword.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$")
+                && adminPhone.matches("([0]|[374]{3}|[+374]{4})([99]|[98]|[97]|[96]|[95]|[94]|[93]" +
+                "|[91]|[77]|[60]|[55]|[44]|[43]|[41]|[33]|[12]|[11]|[10]){2}[0-9]{6}")
+                && adminEmail.matches("^[a-z][a-z0-9-_\\.]+[a-z0-9]+@[a-z]+\\.[a-z]{2,3}")) {
+            Admin newAdmin = new Admin(adminUsername, adminPassword, adminEmail, adminPhone);
+            adminRepository.save(newAdmin);
+            return "Congratulations! " + adminUsername + " registered successfully.";
+        }else
+            return "ERROR 404";
     }
 
     public String editAdmin(Long id, String adminUsername, String adminPassword, String adminEmail, String adminPhone){
