7b1ed72490d31a8845608c8ad634f030b54d0146
artapresyan <apresyanarturv@gmail.com>
2021-10-16 00:22:36 +0400

    added controller for companies

---
+++ src/main/java/com/workspace/workSpace/conroller/CompanyController.java
@@ -0,0 +1,44 @@
+package com.workspace.workSpace.conroller;
+
+import com.workspace.workSpace.entity.Company;
+import com.workspace.workSpace.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("/all")
+    public List<Company> getAllCompanies(){
+        return companyService.getCompanies();
+    }
+
+    @PostMapping("/add")
+    public String addNewCompany(@RequestParam String companyName,@RequestParam String companyEmail,
+                                @RequestParam String companyPhone, @RequestParam String companyOfficeAddress,
+                                @RequestParam String companyUsername, @RequestParam String companyPassword,
+                                @RequestParam(required = false) int numOfEmployees) {
+        return companyService.addCompany(companyName, companyEmail, companyPhone, companyOfficeAddress, companyUsername,
+                companyPassword, numOfEmployees);
+    }
+    @PutMapping("/edit")
+    public String editCompanyInfo(@RequestParam(required = false) String companyName,@RequestParam(required = false) String companyEmail,
+                                  @RequestParam(required = false) String companyPhone, @RequestParam(required = false) String companyOfficeAddress,
+                                  @RequestParam(required = false) String companyUsername, @RequestParam(required = false) String companyPassword,
+                                  @RequestParam(required = false) int numOfEmployees, @RequestParam Long id){
+        return companyService.editCompany(id, companyName, companyEmail, companyPhone, companyOfficeAddress,
+                companyUsername, companyPassword, numOfEmployees);
+    }
+
+    @DeleteMapping("/remove")
+    public String removeCompany(@RequestParam Long id,@RequestParam String password){
+        return companyService.removeCompany(id,password);
+    }
+}
