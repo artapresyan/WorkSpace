568bb46ff1f346afbd7b66baa3faa2c072448602
artapresyan <apresyanarturv@gmail.com>
2021-10-18 21:42:20 +0400

    added controller for employees

---
+++ src/main/java/com/workspace/workSpace/conroller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.workspace.workSpace.conroller;
+
+import com.workspace.workSpace.entity.Employee;
+import com.workspace.workSpace.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getEmployees();
+    }
+
+    @PostMapping("/add")
+    public String addEmployee(@RequestParam String employeeName,@RequestParam String employeeSurname,
+                              @RequestParam String employeeJobCategory,@RequestParam String employeeEmail,
+                              @RequestParam(required = false) String employeePhone,
+                              @RequestParam(required = false) String employeeBirthData,
+                              @RequestParam String employeeUsername,@RequestParam String employeePassword){
+        return employeeService.addEmployee(employeeName, employeeSurname, employeeJobCategory, employeeEmail,
+                employeePhone, employeeBirthData, employeeUsername, employeePassword);
+    }
+
+    @PutMapping("/edit")
+    public String editEmployee(@RequestParam(required = false) String employeeName,@RequestParam(required = false) String employeeSurname,
+                               @RequestParam(required = false) String employeeJobCategory,@RequestParam(required = false) String employeeEmail,
+                               @RequestParam(required = false) String employeePhone, @RequestParam(required = false) String employeeBirthData,
+                               @RequestParam(required = false) String employeeUsername,@RequestParam(required = false) String employeePassword,
+                               @RequestParam Long employeeId){
+        return employeeService.editEmployee(employeeId, employeeName, employeeSurname, employeeJobCategory,
+                employeeEmail, employeePhone, employeeBirthData, employeeUsername, employeePassword);
+    }
+    @DeleteMapping("/remove")
+    public String removeEmployee(@RequestParam Long employeeId, @RequestParam String employeePassword){
+        return employeeService.removeEmployee(employeeId, employeePassword);
+    }
+}
