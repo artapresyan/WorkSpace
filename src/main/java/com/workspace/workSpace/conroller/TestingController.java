0c3fdcc850692eee1f60384677b820437d5cd03d
artapresyan <apresyanarturv@gmail.com>
2021-11-05 16:54:33 +0400

    added view for company registration

--- src/main/java/com/workspace/workSpace/conroller/TestingController.java
+++ src/main/java/com/workspace/workSpace/conroller/TestingController.java
@@ -2,12 +2,19 @@ package com.workspace.workSpace.conroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/registration")
 public class TestingController {
 
-    @GetMapping("/registration/employee")
-    public String registrationModel(){
+    @GetMapping("/employee")
+    public String employeeRegistrationModel(){
         return "employee_registration_view";
+    }
+
+    @GetMapping("/company")
+    public String companyRegistrationModel(){
+        return "company_registration_view";
     }
 }
